567b39a8afcb15a2c52914fac86e24761b5f179c
K N V PHANI KUMAR <119072808+knvphanikumar@users.noreply.github.com>
2023-01-25 23:15:47 +0530

    okay

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0a2457eba250ca23d"
+  ami             = "ami-0cca134ec43cf708f"
   instance_type   = "t2.micro"
-  key_name        = "terrakeypair"
+  key_name        = "phanikey"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1a"
   user_data       = <<EOF
@@ -23,9 +23,9 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0a2457eba250ca23d"
+  ami             = "ami-0cca134ec43cf708f"
   instance_type   = "t2.micro"
-  key_name        = "terrakeypair"
+  key_name        = "phanikey"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1b"
   user_data       = <<EOF
